d95d1b15c50cfb45867c0a145045c21a03ec9e23
andreykor <akorestalev@gmail.com>
2014-12-26 23:00:58 +0300

    Convention version of Stack and Queue

--- assignment/src/main/java/group/idru/assignment/StackWithQueueAlternativ.java
+++ assignment/src/main/java/group/idru/assignment/StackWithQueueAlternativ.java
@@ -1,44 +1,45 @@
-package group.idru.assignment;
+/*
+ * Two queue stack implementation.
+ * Version 1.0
+ */
 
+package group.idru.assignment;
 import java.util.LinkedList;
 
 public class StackWithQueueAlternativ {
     public static void main(String[] args) {
-
         StackClass<String> stackInstance = new StackClass<>(3);
-
         stackInstance.push("1");
         stackInstance.push("2");
         stackInstance.push("3");
         System.out.println(stackInstance);
-        System.out.println(stackInstance.pop()
-        );
-
+        System.out.println(stackInstance.pop());
     }
-
-
 }
 
-
+/**
+ * Stack implementation based on two queues.
+ * This class provides methods for working with.
+ * This implementation is alternative version of StackWithQueue,
+ * it makes some copy job while pushing, to take an object without
+ * any problem.
+ * @param <E>
+ */
 class StackClass<E> {
     private int capacity;
     private LinkedList<E> queueFirst = new LinkedList<>();
     private LinkedList<E> queueSecond = new LinkedList<>();
 
-
     public StackClass(int capacity) {
         this.capacity = capacity;
-
-
     }
 
     public String toString() {
-
         return queueFirst.toString();
     }
 
+    /* Method for putting object to queue */
     public void push(E object) {
-
         if (queueFirst.size() == capacity) {
             throw new IllegalStateException("Stack is full");
         }
@@ -46,33 +47,26 @@ class StackClass<E> {
             queueFirst.add(object);
         } else {
             while (!queueFirst.isEmpty()) {
-
                 queueSecond.add(queueFirst.remove());
             }
             queueFirst.add(object);
             while (!queueSecond.isEmpty()) {
                 queueFirst.add(queueSecond.remove());
-
-
             }
-
-
         }
     }
 
+    /* Method for getting object to queue */
     public E pop() {
-
-        if (queueFirst.isEmpty()) {
+        if (isEmpty()) {
             throw new IllegalStateException("Stack is Empty");
         }
         return queueFirst.remove();
     }
 
     public boolean isEmpty() {
-
         return queueFirst.isEmpty();
     }
-
 }
 
 
