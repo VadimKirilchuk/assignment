b0417393c4337ee516e76980f368076dd47079fb
andreykor <akorestalev@gmail.com>
2015-05-06 22:50:55 +0300

    calc

--- exercises/exercisesjava/src/main/java/exercises/interviewTasks/tsystems/Calculator/CalculatorImpl.java
+++ exercises/exercisesjava/src/main/java/exercises/interviewTasks/tsystems/Calculator/CalculatorImpl.java
@@ -1,8 +1,12 @@
 package exercises.interviewTasks.tsystems.Calculator;
 
-import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Андрей on 03.05.2015.
@@ -11,30 +15,32 @@ public class CalculatorImpl implements Calculator {
     public static void main(String[] args) {
 
         CalculatorImpl calc = new CalculatorImpl();
-        System.out.println(calc.evaluate("3+(2+4*2)+1"));
+        System.out.println(calc.evaluate("-3/2"));
     }
 
     private enum Operator {
-        UNARYMINUS('#', 'u', 3), MULTIPLY('*', 'b', 2), DIVIDE('/', 'b', 2),
-        ADD('+', 'b', 1), SUBTRACT('-', 'b', 1);
+        UNARYMINUS('#', false, 3), MULTIPLY('*', true, 2), DIVIDE('/', true, 2),
+        ADD('+', true, 1), SUBTRACT('-', true, 1), LEFT_PARENTHESISE('(', true, 0),
+        RIGHT_PARENTHESISE(')', true, 0);
+        public static Map<Character, Operator> operatorMap = new HashMap<Character, Operator>();
         private final char operatorSymbol;
         private final int precedence;
-        private char operationStyle;
+        private boolean binary;
 
-        Operator(char operatorSymbol, char operationStyle, int precedence) {
+        static {
+            for (Operator operator : values()) {
+                operatorMap.put(operator.operatorSymbol, operator);
+            }
+        }
+
+        Operator(char operatorSymbol, boolean binary, int precedence) {
             this.operatorSymbol = operatorSymbol;
-            this.operationStyle = operationStyle;
+            this.binary = binary;
             this.precedence = precedence;
         }
 
-        private static Operator getOperator(char token) {
-            Operator[] arrayOfOperators = values();
-            for (int i = 0; i < arrayOfOperators.length; i++) {
-                if (token == arrayOfOperators[i].operatorSymbol) {
-                    return arrayOfOperators[i];
-                }
-            }
-            return null;
+        private static Operator getOperator(Character token) {
+            return operatorMap.get(token);
         }
     }
 
@@ -42,51 +48,77 @@ public class CalculatorImpl implements Calculator {
     public String evaluate(String statement) {
 
         Deque<Double> operandStack = new ArrayDeque<Double>();
-        Deque<Character> operatorStack = new ArrayDeque<Character>();
+        Deque<Operator> operatorStack = new ArrayDeque<Operator>();
         statement = statement.replaceAll("\\s+", "");
 
-        int lengthOfStatement = statement.length();
+        int statementLength = statement.length();
         Character lastProcessed = null;
         try {
-            for (int i = 0; i < lengthOfStatement; i++) {
+            for (int i = 0; i < statementLength; i++) {
                 char token = statement.charAt(i);
 
-                if (token == '(') {
-                    processLeftParenthesis(token, lastProcessed, operatorStack);
-                    lastProcessed = token;
-                } else {
-                    if (token == ')') {
-                        processRightParenthesis(lastProcessed, operatorStack, operandStack);
-                        lastProcessed = token;
-                    } else {
-                        if (isOperator(token)) {
-                            processOperator(token, lastProcessed, operandStack, operatorStack, i,
-                                    lengthOfStatement);
-                            lastProcessed = token;
-                        } else {
-                            if (isOperand(token)) {
-                                i = processOperand(lastProcessed, operandStack, statement,
-                                        i, lengthOfStatement);
-                                lastProcessed = token;
-                            } else {
-                                return null;
-                            }
-                        }
-                    }
-                }
+                i = analyzeToken(token, lastProcessed, i, statement, operatorStack,
+                        operandStack);
+                lastProcessed = token;
             }
-            processFinalExpression(operandStack, operatorStack);
-        } catch (Exception e) {
+
+            processResultExpression(operandStack, operatorStack);
+            String result = getStringRoundResult(operandStack);
+            return result;
+        } catch (Exception e)
+
+        {
             e.printStackTrace();
             return null;
         }
-        double result = operandStack.pollLast();
-        return BigDecimal.valueOf(result).setScale(4, BigDecimal.ROUND_HALF_UP).toString();
     }
 
-    private void processFinalExpression(Deque<Double> operandStack,
-                                        Deque<Character> operatorStack) {
-        while (!operatorStack.isEmpty() && operatorStack.peekLast() != '(') {
+    private int analyzeToken(char token, Character lastProcessed, int tokenIndex,
+                             String statement, Deque<Operator> operatorStack,
+                             Deque<Double> operandStack) {
+        if (isOperator(token)) {
+            int statementLength = statement.length();
+            processOperatorToken(token, lastProcessed, tokenIndex, statementLength,
+                    operatorStack, operandStack);
+            return tokenIndex;
+        } else {
+            if (isOperand(token)) {
+                return processOperand(lastProcessed, operandStack, statement, tokenIndex);
+            } else {
+                throw new IllegalStateException();
+            }
+        }
+    }
+
+    private void processOperatorToken(char token, Character lastProcessed,
+                                      int tokenIndex, int statementLength,
+                                      Deque<Operator> operatorStack,
+                                      Deque<Double> operandStack) {
+        switch (token) {
+            case '(':
+                processLeftParenthesis(token, lastProcessed, operatorStack);
+                break;
+            case ')':
+                processRightParenthesis(lastProcessed, operatorStack, operandStack);
+                break;
+            default:
+                processOperator(token, lastProcessed, operandStack, operatorStack,
+                        tokenIndex, statementLength);
+        }
+    }
+
+    private String getStringRoundResult(Deque<Double> operandStack) {
+        Double result = operandStack.pollLast();
+        NumberFormat numberFormat = DecimalFormat.getInstance();
+        numberFormat.setRoundingMode(RoundingMode.HALF_UP);
+        numberFormat.setMinimumFractionDigits(0);
+        numberFormat.setMaximumFractionDigits(4);
+        return numberFormat.format(result);
+    }
+
+    private void processResultExpression(Deque<Double> operandStack,
+                                         Deque<Operator> operatorStack) {
+        while (!operatorStack.isEmpty() && operatorStack.peekLast() != Operator.LEFT_PARENTHESISE) {
             execute(operatorStack, operandStack);
         }
         if (!operatorStack.isEmpty()) {
@@ -94,26 +126,34 @@ public class CalculatorImpl implements Calculator {
         }
     }
 
-    private int processOperand(Character lastProcessed, Deque<Double> operandStack,
-                               String statement, int tokenIndex, int statementLength) {
+    private boolean isValidOperand(Character lastProcessed) {
+        return Operator.getOperator(lastProcessed) != Operator.RIGHT_PARENTHESISE;
+    }
 
-        //todo:here is my problem, i must make extra checking last symbol on null, because lastProcessed is object,
-        //we can have case where lastProcessed ==null, so operand is first symbol,
-        // and if we will make lastProcessed ==")" we will get error
-        if (lastProcessed == null || lastProcessed != ')') {
+    private int processOperand(Character lastProcessed, Deque<Double> operandStack,
+                               String statement, int tokenIndex) {
+
+        if (isValidOperand(lastProcessed)) {
 
             StringBuilder stringBuilder = new StringBuilder();
+            int statementLength = statement.length();
+            Double number = null;
+
             //loop for   number, walk across the string and append parts of number
             // is this loop good?
-            while (tokenIndex != statementLength && (isOperand(statement.charAt(tokenIndex))
-                    || statement.charAt(tokenIndex) == '.')) {
+
+            while (tokenIndex != statementLength && ((isOperand(statement.charAt(tokenIndex))
+                    || statement.charAt(tokenIndex) == '.'))) {
+
                 stringBuilder.append(statement.charAt(tokenIndex));
                 tokenIndex++;
             }
-            String number = stringBuilder.toString();
+            String stringNumber = stringBuilder.toString();
             //convert to double
-            Double doubleNumber = Double.valueOf(number);
-            operandStack.addLast(doubleNumber);
+
+            number = Double.valueOf(stringNumber);
+
+            operandStack.addLast(number);
         } else {
 
             throw new IllegalArgumentException();
@@ -121,89 +161,110 @@ public class CalculatorImpl implements Calculator {
         return (tokenIndex - 1);
     }
 
+    //todo:сравниваем через char
+    private boolean isValidUnaryOperator(Operator operator, Character lastProcessed) {
+        return (operator == Operator.SUBTRACT) && (lastProcessed == null
+                || lastProcessed == Operator.LEFT_PARENTHESISE.operatorSymbol);
+    }
+
+    private boolean isValidBinaryOperator(Character lastProcessed,
+                                          int tokenIndex, int statementLength) {
+        return (isOperand(lastProcessed) || (lastProcessed != null
+                && (lastProcessed == Operator.RIGHT_PARENTHESISE.operatorSymbol)))
+                && (tokenIndex != statementLength - 1);
+    }
+
     private void processOperator(char token, Character lastProcessed,
                                  Deque<Double> operandStack,
-                                 Deque<Character> operatorStack,
+                                 Deque<Operator> operatorStack,
                                  int tokenIndex, int statementLength) {
-        // todo: don't like this mechanism, to analyze operator we should convert token to enum member
         Operator operator = Operator.getOperator(token);
 
         // if "-" is unary ,"-1+2","(-1+4)"
         //here checking that lastProcessed ==null is necessary, it is OK
-        if ((operator == Operator.SUBTRACT) && (lastProcessed == null || lastProcessed == '(')) {
-            operatorStack.addLast(Operator.UNARYMINUS.operatorSymbol);
+        if (isValidUnaryOperator(operator, lastProcessed)) {
+            operatorStack.addLast(Operator.UNARYMINUS);
         } else {
 
             //if we have binary operation
             //OK - "2+3", "...)+12"
             //NOT OK - "+2"," /2+3 ", "2+3+", "3++5"
-            ///todo:here is my problem, i must make extra checking last symbol on null, because lastProcessed is object
-            if (lastProcessed != null && (isOperand(lastProcessed) || lastProcessed == ')')
-                    && (tokenIndex != statementLength - 1)) {
-                Character stackOperator = operatorStack.peekLast();
+            if (isValidBinaryOperator(lastProcessed, tokenIndex, statementLength)) {
+                Operator stackOperator = operatorStack.peekLast();
                 //if operator precedence <= precedence of last operation at stack
                 // we execute operation till precedence become > than operation from stack or till we find "("
-                while (stackOperator != null && stackOperator != '('
-                        && checkPrecedence(token, stackOperator)) {
+                //&& stackOperator != Operator.LEFT_PARENTHESISE
+                while (stackOperator != null && checkPrecedence(operator, stackOperator)) {
                     execute(operatorStack, operandStack);
                     stackOperator = operatorStack.peekLast();
                 }
                 //add operator to stack
-                operatorStack.addLast(token);
+                operatorStack.addLast(operator);
             } else {
                 throw new IllegalArgumentException();
             }
         }
     }
-//here we process case where our symbol is left parenthesis
+
+    private boolean isValidLeftParenthesis(Character lastProcessed) {
+        return !isOperand(lastProcessed)
+                && (Operator.getOperator(lastProcessed) != Operator.RIGHT_PARENTHESISE);
+    }
+
+    //here we process case where our symbol is left parenthesis
     private void processLeftParenthesis(char token, Character lastProcessed,
-                                        Deque<Character> operatorStack) {
+                                        Deque<Operator> operatorStack) {
         // we work only with operators, "(",")" and numbers, so on previous step symbol could be number,
         // operator or"(", because when we work with ")" we execute expression,  and "(" and operator is good, so
         // we can put to stack "(" only if previous symbol in statement was not number-"(1+3...","1+(..", "((..."
         // not "2(..."
 
-        //todo:here is my problem, i must make extra checking last symbol on null, because lastProcessed is object,
-        //and we can't check null at isOperand(char symb)
-        if (lastProcessed == null || !isOperand(lastProcessed)) {
-            operatorStack.addLast(token);
+        if (isValidLeftParenthesis(lastProcessed)) {
+            Operator operator = Operator.getOperator(token);
+            operatorStack.addLast(operator);
         } else {
             throw new IllegalArgumentException();
         }
     }
+
+    private boolean isValidRightParenthesis(Character lastProcessed) {
+        return isOperand(lastProcessed)
+                || Operator.getOperator(lastProcessed) == Operator.RIGHT_PARENTHESISE;
+    }
+
     //here we process case where our symbol is left parenthesis
     private void processRightParenthesis(Character lastProcessed,
-                                         Deque<Character> operatorStack,
+                                         Deque<Operator> operatorStack,
                                          Deque<Double> operandStack) {
         //we can use ")" only if last symbol was number or "(", good situation-"..8)","...))"
         //not good "..+)"
 
-        //todo:here is my problem, i must make extra checking last symbol on null, because lastProcessed is object,
-        if (lastProcessed != null && isOperand(lastProcessed)) {
+        if (isValidRightParenthesis(lastProcessed)) {
 
             //while stack is not empty we should search "(", and make some work with one operator
             // and one or two numbers, after execute expression we should put result to operand stack
-            while (!operatorStack.isEmpty() && operatorStack.peekLast() != '(') {
+            while (!operatorStack.isEmpty() && operatorStack.peekLast() != Operator.LEFT_PARENTHESISE) {
                 execute(operatorStack, operandStack);
             }
             // if we didnt find "(" we report about error, because we haven't pair for ")"
-            if (operatorStack.isEmpty()) {
-                throw new IllegalArgumentException();
+            if (!operatorStack.isEmpty()) {
+                operatorStack.pollLast();
             } else {
                 // if everything is great we remove "(" from stack
-                operatorStack.pollLast();
+
+                throw new IllegalArgumentException();
             }
         } else {
             throw new IllegalArgumentException();
         }
     }
 
-    private void execute(Deque<Character> operatorStack,
+    private void execute(Deque<Operator> operatorStack,
                          Deque<Double> operandStack) {
-        Character stackOperator = operatorStack.pollLast();
-        Operator operator = Operator.getOperator(stackOperator);
+        Operator operator = operatorStack.pollLast();
+
         Double result = null;
-        if (operator.operationStyle == 'u') {
+        if (!operator.binary) {
             Double operand = operandStack.pollLast();
             result = executeOperation(operator, operand, null);
         } else {
@@ -232,22 +293,27 @@ public class CalculatorImpl implements Calculator {
         }
     }
 
-
-    private boolean checkPrecedence(char token, char stackOperator) {
-        Operator operatorForToken = Operator.getOperator(token);
-        Operator operatorForStackOperator = Operator.getOperator(stackOperator);
-        return operatorForToken.precedence <= operatorForStackOperator.precedence;
+    private boolean checkPrecedence(Operator token, Operator stackOperator) {
+        return token.precedence <= stackOperator.precedence;
     }
 
     private boolean isOperand(char token) {
 
-        return (token >= '0' && token <= '9');
+        return token >= '0' && token <= '9';
+    }
+
+    private boolean isOperand(Character token) {
+        return token != null && isOperand(token.charValue());
     }
 
     private boolean isOperator(char token) {
         Operator operator = Operator.getOperator(token);
         //any operator is good for us except unary, because unary is our shadowside operator,
         // we analyze binary operator and if it satisfy our condition then we use unary version
-        return (operator != null) && (operator.operationStyle != 'u');
+        return (operator != null) && operator.binary;
+    }
+
+    private boolean isOperator(Character token) {
+        return token != null && isOperator(token.charValue());
     }
 }
---
+++ exercises/exercisesjava/src/main/java/exercises/interviewTasks/tsystems/Calculator/Test.java
@@ -0,0 +1,27 @@
+package exercises.interviewTasks.tsystems.Calculator;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+/**
+ * Created by Андрей on 05.05.2015.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(format(14.0184849945)); // prints '14.01'
+        System.out.println(format(13.00011)); // prints '13'
+        System.out.println(format(3.5)); // prints '3.5'
+        System.out.println(format(3.138136)); // prints '3.13'
+    }
+
+    public static String format(Number n) {
+        NumberFormat format = DecimalFormat.getInstance();
+        format.setRoundingMode(RoundingMode.FLOOR);
+        format.setMinimumFractionDigits(0);
+        format.setMaximumFractionDigits(2);
+        return format.format(n);
+    }
+
+}
